fb911f0e99edf7672d2eb528637deab5e10c95fc
Owen Xia <owenyx@gmail.com>
2015-05-04 01:17:13 +0800

    Cancan bug fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
-	load_and_authorize_resource param_method: :comment_params
-
+	load_and_authorize_resource
+	
+	# Apparently load_and_authorize_resources does not load @annotation
+	before_action :set_annotation, only: [:edit, :update, :destroy]
+	
 	def create
 		@annotation = Annotation.all.find(params[:annotation_id])
 		@comment = @annotation.comments.create(comment_params)
 		@comment.user_id = current_user.id
+		@comment.save
 		redirect_to annotation_path(@annotation)
 	end
 
@@ -30,6 +34,10 @@ class CommentsController < ApplicationController
 	private
 		def comment_params
 			params.require(:comment).permit(:name, :body)
+		end
+
+		def set_annotation
+		 	@annotation = Annotation.all.find(params[:annotation_id])
 		end
 
 end
